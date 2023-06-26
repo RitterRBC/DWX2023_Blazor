e0793580dff00e72c9e30a27a121e2dc11eeddd7
Teilnehmer <thorsten.daempfert@gmail.com>
2023-06-26 15:35:24 +0200

    2023-06-26 Blazor Server component sample

---
+++ BlazorConsole/BlazorConsole/Pages/_host.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorConsole.Pages
+{
+    public class _hostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
